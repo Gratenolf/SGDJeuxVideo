370249b109dfb7f6ea217ba1138e987dc01cf0cf
Nicolas GODINHO <ng452532@MI114-01.iem>
2018-01-19 14:26:18 +0100

    jigpjiojijio :wq
    
    
     commençant par '#' seront ignorées, et un message vide abandonne la validation.

--- RequetesP2.js
+++ RequetesP2.js
@@ -1,8 +1,8 @@
 //1 Retourne l'ensemble des jeux d'action ou d'aventure
-db.JeuxVideo.find({$or:[{"categorie":"action"},{"categorie":"aventure"}]});
+db.JeuxVideo.find({$or:[{"categorie":"Action"},{"categorie":"Aventure"}]});
 
 //2 retourne l'ensemble des jeux d'action ayant un prix inférieur ou égale à 30
-db.JeuxVideo.find({$and[{"categorie":"action"},{"prix":{$lte:30}]}});
+db.JeuxVideo.find({$and:[{"categorie":"Action"},{"prix":{$lte:30}}]});
 
 //3 retourne le nombre total de jeux ayant un prix supérieur ou égal à 30
 db.JeuxVideo.find({"prix":{$gte:30}}).count();
@@ -29,38 +29,34 @@ db.JeuxVideo.distinct("auteur");
 db.JeuxVideo.aggregate({$match:{"Joueurs.pseudo":{$ne:null}}},{$unwind:"$Joueurs"},{$group:{_id:"$Joueurs.pseudo",total:{$sum:1}}},{$sort:{total:-1}},{$limit:3});
 
 //11 Lister les jeux suivant leurs moyennes par ordre décroissant
-
+db.JeuxVideo.aggregate({$unwind:"$Joueurs"},{$match:{"Joueurs.notes":{$ne:null}}},{$group:{_id:"$nom",moy:{$avg:"$Joueurs.notes"}}},{$sort:{moy:-1}});
 
 //12 Afficher le top 3 des jeux possedant les meilleurs moyennes
-var map = function(){
-	var noteMoy = {note:this.Joueurs.notes,count: 1};
-	emit(this.nom,noteMoy);
-};
-
-var reduce = function(jeu,noteMoy){
-	reduceNoteMoy = {note : 0, count : 0};
-	for(var i=0;i<noteMoy.length;i++){
-		reduceNoteMoy.note += noteMoy[i].note;
-		reduceNoteMoy.count += val[i].count;
-	}
-	reduceNoteMoy.avg = reduceNoteMoy.note / reduceNoteMoy.count;
-	return reduceNoteMoy;
-};
-
-db.JeuxVideo.mapReduce(map, reduce, {out:{inline:1}});
-
-
-
 db.JeuxVideo.aggregate({$unwind:"$Joueurs"},{$match:{"Joueurs.notes":{$ne:null}}},{$group:{_id:"$nom",moy:{$avg:"$Joueurs.notes"}}},{$sort:{moy:-1}},{$limit:3});
+
 
 //13 Afficher les jeux d'action possedant un avis de "SuperFrost" ou "c3l1n3" et une note supérieur à 8
 
 
 //14 Compter le nombre de jeux par auteurs
+var map = function(){
+	var nbJeu = {count : 1};
+	emit(this.auteur,nbJeu);
+};
 
+var reduce = function(auteur,jeux){
+	reduceNbJeux = {count : 0};
+	for(var i=0;i<jeux.length;i++){
+		reduceNbJeux.count += jeux[i].count;
+	}
+	return reduceNbJeux;
+};
+
+db.JeuxVideo.mapReduce(map, reduce, {out:{inline:1}});
 
 
 //15 Compter le nombre d'avis par joueur et leurs note maximal attribuée
 
 
 //16 Compter le nombre de jeux par catégorie
+db.JeuxVideo.aggregate({$match:{"categorie":{$ne:null}}},{$unwind:"$categorie"},{$group:{_id:"$categorie",count:{$sum:1}}});
