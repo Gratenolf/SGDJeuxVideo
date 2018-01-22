ba3fd59af8dc9294e883d2eb50ee4a20000ef9c2
Nicolas GODINHO <ng452532@MI114-01.iem>
2018-01-22 13:26:09 +0100

    mise en forme pymongo

--- p3.py
+++ p3.py
@@ -6,7 +6,7 @@ db = c.ng452532
 
 jv = db.JeuxVideo
 
-
+print("\nAffichage du top 3 des jeux possedant les meilleurs moyennes : \n")
 pipeline = [{"$unwind":"$joueurs"},
 			{"$match":{"joueurs.notes":{"$ne":"null"}}},
 			{"$group":{"_id":"$nom","moy":{"$avg":"$joueurs.notes"}}},
@@ -15,26 +15,24 @@ pipeline = [{"$unwind":"$joueurs"},
 ]
 pprint.pprint(list(jv.aggregate(pipeline)))
 
+print("\nAffichage du nombre de jeux disponible par catégorie : \n")
 pipeline2 = [{"$match":{"categorie":{"$ne":"null"}}},
 			{"$unwind":"$categorie"},
 			{"$group":{"_id":"$categorie","count":{"$sum":1}}}
 ]
-
 pprint.pprint(list(jv.aggregate(pipeline2)))
 
-
+print("\nAffichage de l'ensemble de la collection : \n")
 for i in jv .find() : i
 
-
-
-
+print("\nAffichage du nombre d'avis par joueur et leurs note maximal attribuée : \n")
 pipeline3 = [{"$unwind":"$joueurs"},
 			{"$match":{"joueurs.note":{"$ne":"null"}}},
 			{"$group":{"_id":"$joueurs.pseudo","total":{"$sum":1}}}
 ]
 pprint.pprint(list(jv.aggregate(pipeline3)))
 
-
+print("\nAffichage du top 3 des meilleurs commentateurs : \n")
 pipeline4 = [{"$match":{"joueurs.pseudo":{"$ne":"null"}}},
 			{"$unwind":"$joueurs"},
 			{"$group":{"_id":"$joueurs.pseudo","total":{"$sum":1}}},
@@ -43,15 +41,9 @@ pipeline4 = [{"$match":{"joueurs.pseudo":{"$ne":"null"}}},
 ]
 pprint.pprint(list(jv.aggregate(pipeline4)))
 
-
-
-
-cursor = jv.aggregate([{"$match":{"categorie":{"$ne":"null"}}},{"$unwind":"$categorie"},{"$group":{"_id":"$categorie","count":{"$sum":1}}}])
-print(list(cursor))
-
+print("\nAffichage de l'ensemble des auteurs : \n")
 cursorb = jv.distinct("auteur")
 print(cursorb)
 
-
-
+print("\nAffichage des jeux de l'auteur EA : \n")
 for doc in jv .find({"auteur":"EA"}):doc
