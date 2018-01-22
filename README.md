d9de1f193d259bd66446cacea3d63d98403dd481
Adam HADDOU <ah154489@MI114-01.iem>
2018-01-22 10:09:16 +0100

    qdqsdcvsdv

--- README.md
+++ README.md
@@ -13,10 +13,6 @@ mongoimport --db USER --collection JeuxVideo --file JeuxVideo.json
 -Connexion MongoDB:
 mongo –u nomlogin –p
 use nomlogin
-<<<<<<< HEAD
->>>>>>> cf9c8e4694034508f45d632f1bbbf640976a5fb0
 
 $where
 https://www.mkyong.com/mongodb/mongodb-find-all-documents-where-an-array-list-size-is-greater-than-n/
-=======
->>>>>>> 8389beb28d0ca442bd4fffd805c466e98ed18a91
--- RequetesP2.js
+++ RequetesP2.js
@@ -11,21 +11,21 @@ db.JeuxVideo.find({"prix":{$gte:30}}).count();
 
 
 //4 suppression du pseudo "DarkSasuke91"
-db.Projet.remove({"Joueurs.pseudo":"DarkSasuke91"});
+db.JeuxVideo.remove({"Joueurs.pseudo":"DarkSasuke91"});
 
 //5 Afficher la liste complete des catégories existantes
 db.JeuxVideo.distinct("categorie");
 
 
 //6 Afficher les 3 premiers jeux d'un auteur
-db.Projet.find({"auteur":"EA"}).limit(3);
+db.JeuxVideo.find({"auteur":"EA"}).limit(3);
 
 //7 Afficher l'ensemble des auteurs existants
 db.JeuxVideo.distinct("auteur");
 
 
 //8 Afficher les noms des jeux possedant plus de 3 avis
-db.Projet.find({Joueurs:{$exists:true},$where:'this.Joueurs.length > 3'});
+db.JeuxVideo.find({Joueurs:{$exists:true},$where:'this.Joueurs.length > 3'});
 
 //9 Afficher le top 3 des joueurs donnant un avis
 db.JeuxVideo.aggregate({$match:{"Joueurs.pseudo":{$ne:null}}},{$unwind:"$Joueurs"},{$group:{_id:"$Joueurs.pseudo",total:{$sum:1}}},{$sort:{total:-1}},{$limit:3});
@@ -40,21 +40,21 @@ db.JeuxVideo.aggregate({$unwind:"$Joueurs"},{$match:{"Joueurs.notes":{$ne:null}}
 
 
 //12 Afficher les jeux d'action possedant un avis de "SuperFrost" ou "c3l1n3" et une note supérieur à 8
-db.Projet.aggregate({$match:{$and:[{$or:[{"Joueurs.pseudo":"SuperFrost","Joueurs.pseudo":"c3l1n3"}]},{"Joueurs.note":{$gte:8}}]}});
+db.JeuxVideo.aggregate({$match:{$and:[{$or:[{"Joueurs.pseudo":"SuperFrost","Joueurs.pseudo":"c3l1n3"}]},{"Joueurs.note":{$gte:8}}]}});
 
 //13 Compter le nombre de jeux par auteurs
-var map = function(){
+var mapR = function(){
 	var nbJeu = {count : 1};
 	emit(this.auteur,nbJeu);
 };
-var reduce = function(auteur,jeux){
+var reduceR = function(auteur,jeux){
 	reduceNbJeux = {count : 0};
 	for(var i=0;i<jeux.length;i++){
 		reduceNbJeux.count += jeux[i].count;
 	}
 	return reduceNbJeux;
 };
-db.JeuxVideo.mapReduce(map, reduce, {out:{inline:1}});
+db.JeuxVideo.mapReduce(mapR, reduceR, {out:{inline:1}});
 
 
 //14 Compter le nombre d'avis par joueur et leurs note maximal attribuée
@@ -71,5 +71,49 @@ db.JeuxVideo.updateMany({"categorie":"Action"},{$push:{"Joueurs":{"pseudo":"test
 //17 renommer l'auteur IA en EA
 db.JeuxVideo.replaceOne({"auteur":"IA"},{"auteur":"EA"});
 
+//18 prix moyen par éditeur
+var map = function(){
+	var pJeu = {pt:this.prix,count : 1};
+	emit(this.auteur,pJeu);
+};
+var reduce = function(auteur,pJeux){
+	reducePrix = {pt:0,count : 0};
+	for(var i=0;i<pJeux.length;i++){
+		reducePrix.pt += pJeux[i].pt;
+		reducePrix.count+=pJeux[i].count;
+	}
+	reducePrix.avg = reducePrix.pt/reducePrix.count;
+	return reducePrix;
+};
+db.JeuxVideo.mapReduce(map, reduce, {out:{inline:1}});
+
+//19 Compter avis pour chaque pseudo
+var map = function(){
+	var TestFinBoucle = this.Joueurs.length;
+	for(var idx=0;idx < this.Joueurs.length;idx++){
+		var pseudo = this.Joueurs[idx].pseudo;
+		var avis = {count:1,avis:this.Joueurs[idx].avis}
+		emit(pseudo,avis);
+	}
+};
+
+var reduce = function(pseudo,avis){
+	reduceAvis = {count:0,avis:0};
+	
+	for(var idx=0;idx<avis.length;idx++){
+		reduceAvis.count+=avis[idx].count;
+	}
+	return reduceAvis;
+};
+
+db.JeuxVideo.mapReduce(map, reduce, {out:{inline:1}});
 
 
+//-------------------
+var map = function(){
+	Joueurs.forEach(function(P)){
+		emit({count:1,P[0]},);
+	}
+}
+
+var reduce = function()
